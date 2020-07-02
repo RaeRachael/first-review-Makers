25d96e4e6f60b241957dab1c880244acd95d2aa2
Rachael <rachaelewins@hotmail.com>
2020-07-02 15:34:24 +0100

    single grade type, can count Reds, should return 3

--- lib/grade_report.rb
+++ lib/grade_report.rb
@@ -2,12 +2,15 @@ def grade_report(string)
   grades = string.split(', ')
   green = 0
   amber = 0
+  red = 0
   grades.each do |grade|
     green += 1 if grade == "Green"
     amber += 1 if grade == "Amber"
+    red += 1 if grade == "Red"
   end
 
   return "Green: #{green}\nAmber: #{amber}" if green > 0 && amber > 0
   return "Green: #{green}" if green > 0
   return "Amber: #{amber}" if amber > 0
+  return "Red: #{red}"
 end
--- spec/grade_report_spec.rb
+++ spec/grade_report_spec.rb
@@ -15,11 +15,23 @@ describe '#grade_report' do
         expect(grade_report("Amber")).to eq("Amber: 1")
       end
     end
+    describe "can count Reds" do
+      it "should return 'Reds: 3'" do
+        input_string = "Red, Red, Red"
+        output_string = "Red: 3"
+        expect(grade_report(input_string)).to eq(output_string)
+      end
+    end
   end
   describe "two grade types" do
     describe "mix of Green and Amber" do
       it "should return 'Green: 1\\nAmber: 1' " do
         expect(grade_report("Green, Amber")).to eq("Green: 1\nAmber: 1")
+      end
+    end
+    describe "mix of Green and Red" do
+      xit "should return 'Green: 1\\nRed: 1' " do
+        expect(grade_report("Green, Red")).to eq("Green: 1\nRed: 1")
       end
     end
   end
